508638aa953e08bba2679cf106febe9e2bd55bd9
muhammadamir98 <98.muhammadamir@gmail.com>
2021-12-14 19:11:43 +0330

    ProductCategory Managment Implemented

---
+++ Lampshade/0_Framework/Application/OperationResult.cs
@@ -0,0 +1,27 @@
+﻿namespace _0_Framework.Application
+{
+    public class OperationResult
+    {
+        public string Message { get; set; }
+        public bool IsSuccedded { get; set; }
+
+        public OperationResult()
+        {
+            IsSuccedded = false;
+        }
+
+        public OperationResult Succedded(string message = "عملیات با موفقیت انجام شد.")
+        {
+            IsSuccedded = true;
+            Message = message;
+            return this;
+        }
+
+        public OperationResult Failed(string message)
+        {
+            IsSuccedded = false;
+            Message = message;
+            return this;
+        }
+    }
+}
---
+++ Lampshade/ShopManagmentConfiguration/ShopManagmentBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore;
+using ShopManagement.Infrastructure.EFCore.Repository;
+
+namespace ShopManagementConfiguration
+{
+    public class ShopManagmentBootstrapper
+    {
+        public static void Configure(IServiceCollection service, string connectionString)
+        {
+            service.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+            service.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+            service.AddDbContext<ShopContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
